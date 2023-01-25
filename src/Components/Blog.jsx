4128598e0751ae0bb56fe516381c4b5f088d267c
Tejas Nikam <Tejasnikam4515@gmail.com>
2023-01-25 23:16:34 +0530

    updated

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -9,7 +9,7 @@ function Blog() {
     <CCol></CCol>
     <CCol xs={10}>
     <CRow className="text-center bg-info rounded-pill pt-5 pb-5">
-    <CCol  xs={4}> <h5 className="text-center text-light ">Subscribe Newsletters</h5></CCol>
+    <CCol  xs={4}> <h5 className="text-center text-light mt-2 ">Subscribe Newsletters</h5></CCol>
     <CCol  xs={4}> <input className="form-control rounded-pill" type="text" placeholder="Enter your email id." /></CCol>
     <CCol  xs={2}><button type="submit" className="d-grid gap-2 md-3  mx-auto btn btn-outline-light rounded-pill ml-2">Subscribe</button></CCol>
     </CRow>
