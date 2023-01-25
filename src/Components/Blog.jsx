c7a6adf39cd4ec19d24d76e5fc34de3530d3bb81
Tejas Nikam <Tejasnikam4515@gmail.com>
2023-01-25 17:49:36 +0530

    code changed

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -8,16 +8,17 @@ function Blog() {
   <CRow>
     <CCol></CCol>
     <CCol xs={10}>
-    <CRow className="text-center bg-info rounded-pill pt-4 pb-4">
-    <CCol  xs={4}> <h5 className="text-center " >Subscribe Newsletters</h5></CCol>
+    <CRow className="text-center bg-info rounded-pill pt-5 pb-5">
+    <CCol  xs={4}> <h5 className="text-center text-light ">Subscribe Newsletters</h5></CCol>
     <CCol  xs={4}> <input className="form-control rounded-pill" type="text" placeholder="Enter your email id." /></CCol>
-    <CCol  xs={2}><button type="submit" className="d-grid gap-2  mx-auto btn btn-outline-light rounded-pill ml-2">Subscribe</button></CCol>
+    <CCol  xs={2}><button type="submit" className="d-grid gap-2 md-3  mx-auto btn btn-outline-light rounded-pill ml-2">Subscribe</button></CCol>
     </CRow>
     </CCol>
     <CCol></CCol>
   </CRow>
 
 </CContainer>
+
     </div>
   );
 }
